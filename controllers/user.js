d74d76ab9c6e32c9ded88a84f2afaea1f95f62c7
TASKMASTER682 <sayedanwarulhaq@gmail.com>
2020-11-05 15:58:16 +0530

    deploy

--- controllers/user.js
+++ controllers/user.js
@@ -27,9 +27,9 @@ exports.publicProfile = (req, res) => {
          Blog.find({ postedBy: userId })
             .populate('categories', '_id name slug')
             .populate('tags', '_id name slug')
-            .populate('postedBy', '_id name')
+            .populate('postedBy', '_id name facebook linkedin insta twitter about')
             .limit(10)
-            .select('_id title slug excerpt categories tags postedBy createdAt updatedAt')
+            .select('_id title slug excerpt categories tags postedBy createdAt updatedAt ')
             .exec((err, data) => {
                 if (err) {
                     return res.status(400).json({
